1acd31e98e79b052d42af0cd47813d87234f33bb
Susanne Trygg <susanne.trygg@gmail.com>
2020-04-10 21:08:53 +0200

    last fixes

--- src/js/script.js
+++ src/js/script.js
@@ -11,27 +11,23 @@ document.getElementById("home").addEventListener("click", Home);
 
 
 
+function Home() {
+    $("#content-box").empty();
+    $(homeDiv).appendTo("#content-box");
+}
 
 function Affarsplan() {
-    console.log("Du klickade på affärsidé");
     $("#content-box").empty();
     $(affarsplan).appendTo("#content-box");
 }
 
 function Projektide() {
-    console.log("Du klickade på projektidé");
     $("#content-box").empty();
     $(projektDiv).appendTo("#content-box");
 }
 
 function Inspiration() {
-    console.log("Du klickade på Inspiration");
     $("#content-box").empty();
     $(inspirationDiv).appendTo("#content-box");
 }
 
-function Home() {
-    console.log("Du är hemma!");
-    $("#content-box").empty();
-    $(homeDiv).appendTo("#content-box");
-}
